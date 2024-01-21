feaccab0d83de8ef938251ae074e9f65dfb8f6c1
julianthant <julianthant@gmail.com>
2024-01-21 01:17:16 -0800

    Add pusher notification for incoming messages

--- src/app/api/message/send/route.ts
+++ src/app/api/message/send/route.ts
@@ -4,6 +4,8 @@ import { db } from '@/lib/db';
 import { getServerSession } from 'next-auth';
 import { nanoid } from 'nanoid';
 import { Message, messageValidator } from '@/lib/validations/message';
+import { pusherServer } from '@/lib/pusher';
+import { toPusherKey } from '@/lib/utils';
 
 export async function POST(req: Request) {
   try {
@@ -49,6 +51,14 @@ export async function POST(req: Request) {
     };
 
     const message = messageValidator.parse(messageData);
+
+    // notify all connected chat room clients
+
+    pusherServer.trigger(
+      toPusherKey(`chat:${chatId}`),
+      'incoming-message',
+      message
+    );
 
     await db.zadd(`chat:${chatId}:messages`, {
       score: timestamp,
