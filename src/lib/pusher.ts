e270daa0587561948f21729750f5f97bc7fffbdf
julianthant <julianthant@gmail.com>
2024-01-21 05:20:23 -0800

    Update Pusher configuration

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -1,17 +1,17 @@
-import PusherServer from 'pusher'
-import PusherClient from 'pusher-js'
+import PusherServer from 'pusher';
+import PusherClient from 'pusher-js';
 
 export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
-  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
-  secret: process.env.PUSHER_APP_SECRET!,
-  cluster: 'eu',
+  key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
+  secret: process.env.PUSHER_SECRET!,
+  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
   useTLS: true,
-})
+});
 
 export const pusherClient = new PusherClient(
-  process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+  process.env.NEXT_PUBLIC_PUSHER_KEY!,
   {
-    cluster: 'eu',
+    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
   }
-)
+);
