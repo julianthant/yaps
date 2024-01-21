8347a55114a711dfb04fdc7823a690bc9bedc007
julianthant <julianthant@gmail.com>
2024-01-21 01:17:14 -0800

    Add pusher notification for incoming friend requests

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from '@/helpers/redis';
 import { authOptions } from '@/lib/auth';
 import { db } from '@/lib/db';
+import { pusherServer } from '@/lib/pusher';
+import { toPusherKey } from '@/lib/utils';
 import { addFriendValidator } from '@/lib/validations/add-friend';
 import { getServerSession } from 'next-auth';
 import { z } from 'zod';
@@ -73,7 +75,17 @@ export async function POST(req: Request) {
       });
     }
 
-    //valid request
+    //valid request, send friend request
+
+    pusherServer.trigger(
+      toPusherKey(`user:${idToAdd}:incoming_friend_requests`),
+      'incoming_friend_requests',
+      {
+        id: session.user.id,
+        senderEmail: session.user.email,
+        senderName: session.user.name,
+      }
+    );
 
     db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id);
 
