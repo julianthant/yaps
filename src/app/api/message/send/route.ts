4b564c56176b4d8e0b289af14d4b6f08afcaa351
julianthant <julianthant@gmail.com>
2024-01-21 02:22:11 -0800

    Add pusher trigger for new messages

--- src/app/api/message/send/route.ts
+++ src/app/api/message/send/route.ts
@@ -60,6 +60,12 @@ export async function POST(req: Request) {
       message
     );
 
+    pusherServer.trigger(toPusherKey(`user:${friendId}:chats`), 'new_message', {
+      ...message,
+      senderImg: sender.image,
+      senderName: sender.name,
+    });
+
     await db.zadd(`chat:${chatId}:messages`, {
       score: timestamp,
       member: JSON.stringify(message),
