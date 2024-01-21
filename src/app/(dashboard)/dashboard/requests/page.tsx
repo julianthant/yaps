d3e9fb6b1b2abe13c2cd4d112b0dbbaa8b9772c5
julianthant <julianthant@gmail.com>
2024-01-21 04:51:44 -0800

    Refactor friend requests page and update UI

--- src/app/(dashboard)/dashboard/requests/page.tsx
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -1,5 +1,5 @@
-import { fetchRedis } from '@/helpers/redis';
 import FriendRequests from '@/components/FriendRequests';
+import { fetchRedis } from '@/helpers/redis';
 import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 import { notFound } from 'next/navigation';
@@ -9,18 +9,19 @@ const page = async () => {
   const session = await getServerSession(authOptions);
   if (!session) notFound();
 
-  //ids of people who sent us a friend requests
+  // ids of people who sent current logged in user a friend requests
   const incomingSenderIds = (await fetchRedis(
     'smembers',
     `user:${session.user.id}:incoming_friend_requests`
   )) as string[];
 
   const incomingFriendRequests = await Promise.all(
-    incomingSenderIds.map(async (id) => {
-      const sender = (await fetchRedis('get', `user:${id}`)) as string;
+    incomingSenderIds.map(async (senderId) => {
+      const sender = (await fetchRedis('get', `user:${senderId}`)) as string;
       const senderParsed = JSON.parse(sender) as User;
+
       return {
-        id,
+        senderId,
         senderEmail: senderParsed.email,
         senderName: senderParsed.name,
       };
@@ -28,8 +29,8 @@ const page = async () => {
   );
 
   return (
-    <main className="pt-8 h-screen bg-white">
-      <h1 className="font-bold text-5xl mb-8 text-black">Friend requests</h1>
+    <main className="pt-8">
+      <h1 className="font-bold text-5xl mb-8">Add a friend</h1>
       <div className="flex flex-col gap-4">
         <FriendRequests
           incomingFriendRequests={incomingFriendRequests}
